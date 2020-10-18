46577a722da2669a4bccf5a7fec146c3396174c9
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-18 00:28:05 +0000

    Implemented the reset function once the game has a winner to reset the game.

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ const dice2 = document.getElementById("player2Dice")
 const roll = document.getElementById("rollBtn")
 const reset = document.getElementById("resetBtn")
 
+function showResetBtn (){
+  rollBtn.style.display = "none"
+  resetBtn.style.display = "inline-block"
+}
+
 /* Event listener to the Roll Dice Button. Then we generate a random number betwen 1 to 6*/
  
  rollBtn.addEventListener("click", function(){
@@ -24,23 +29,39 @@ const reset = document.getElementById("resetBtn")
        dice1.classList.remove("active")
        dice2.classList.add("active")
        messageDisplay.innerText = ("Player 2 Turn")
-     } else {
-       player2Score += diceRoll
-       player2Scoreboard.textContent = player2Score
-       dice2.innerText = diceRoll
-       dice2.classList.remove("active")
-       dice1.classList.add("active")
-       messageDisplay.innerText = ("Player 1 Turn")
+       } else {
+         player2Score += diceRoll
+         player2Scoreboard.textContent = player2Score
+         dice2.innerText = diceRoll
+         dice2.classList.remove("active")
+         dice1.classList.add("active")
+         messageDisplay.innerText = ("Player 1 Turn")
      }
 
-     if (player1Score >= 20 || player2Score >= 20){
-        rollBtn.style.display = "none"
-        resetBtn.style.display = "inline-block"
-        if (player1Score >= 20){
-            message.textContent = "Player 1 Wins"
-        } else if (player2Score >= 20){
-            message.textContent = "Player 2 Wins"
+     if (player1Score >= 20){
+       showResetBtn()
+       message.textContent = "Player 1 Wins"
+       } else if (player2Score >= 20){
+        showResetBtn()
+        message.textContent = "Player 2 Wins"
         }
     // instead of setting the value inside the if or else we can also flip the value to it's oposite as it is a binary value.
      player1Turn = !player1Turn
  })
+
+
+ resetBtn.addEventListener("click", function(){
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    message.textContent = "Player 1 Turn"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    player1Scoreboard.textContent = player1Score
+    player2Scoreboard.textContent = player2Score
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    rollBtn.style.display = "inline-block"
+    resetBtn.style.display = "none"
+    
+})
